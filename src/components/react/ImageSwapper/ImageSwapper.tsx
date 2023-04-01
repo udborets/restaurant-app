3acf55bab66847775556464d686e82b51cb7bf15
Yury Borets <udborets@gmail.com>
2023-04-01 14:12:32 +0300

    fix image swapper styles

--- src/components/react/ImageSwapper/ImageSwapper.tsx
+++ src/components/react/ImageSwapper/ImageSwapper.tsx
@@ -14,13 +14,14 @@ const ImageSwapper = ({ picturesPaths }: ImageSwapperProps) => {
     setCount((prevCount) => prevCount + 1)
   }, 7000)
   return (
-    <div className={`overflow-hidden`}>
+    <div className={`overflow-hidden w-full h-[100vh] relative`}>
       {picturesPaths.map((src) => (
         <img
+          loading="lazy"
           src={`src/assets/images/${src}.jpg`}
           alt="pic"
           key={src}
-          className={`max-h-[100vh] max-w-1/2 w-1/2 absolute top-0 right-0 ${src === count ? 'opacity-100' : 'opacity-0'} duration-[1.5s] transition-all `}
+          className={`z-[-5] h-auto w-full absolute top-0 right-0 ${src === count ? 'opacity-100' : 'opacity-0'} duration-[1.3s] transition-all `}
         />
       ))}
     </div>
