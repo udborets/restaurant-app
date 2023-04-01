55741fcc2c6905ac460d50e3dd245035380f7da8
Yury Borets <udborets@gmail.com>
2023-04-01 15:08:33 +0300

    fix image swapping component styles

--- src/components/react/ImageSwapper/ImageSwapper.tsx
+++ src/components/react/ImageSwapper/ImageSwapper.tsx
@@ -5,7 +5,7 @@ interface ImageSwapperProps {
 }
 
 const ImageSwapper = ({ picturesNames }: ImageSwapperProps) => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(picturesNames[0]);
   setTimeout(() => {
     if (count === picturesNames.at(-1)) {
       setCount(1)
@@ -14,16 +14,21 @@ const ImageSwapper = ({ picturesNames }: ImageSwapperProps) => {
     setCount((prevCount) => prevCount + 1)
   }, 5000)
   return (
-    <div className={`overflow-hidden w-full h-[100vh] relative`}>
+    <div className={`imageSwapper overflow-hidden w-full h-[100vh] relative`}>
       {picturesNames.map((src) => (
         <img
           loading="lazy"
           src={`src/assets/images/imageSwapper/${src}.jpg`}
           alt="pic"
           key={src}
-          className={`z-[0] h-auto w-full absolute top-0 right-0 ${src === count ? 'opacity-100' : 'opacity-0'} duration-[1.3s] transition-all `}
+          className={`imageSwapper__img z-[0] h-auto w-full absolute top-0 right-0 ${src === count ? 'opacity-100' : 'opacity-0'} duration-[1.3s] transition-all `}
         />
       ))}
+      <div className="imageSwapper imageSelectList">
+        <div className="imageSwapper__i">
+
+        </div>
+      </div>
     </div>
   )
 }
