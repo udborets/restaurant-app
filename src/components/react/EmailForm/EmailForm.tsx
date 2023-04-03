8cd3e1203a941d36f4355a9f21e72edfe40bd0ac
Yury Borets <udborets@gmail.com>
2023-04-03 21:29:37 +0300

    add basic input fields

--- src/components/react/EmailForm/EmailForm.tsx
+++ src/components/react/EmailForm/EmailForm.tsx
@@ -1,7 +1,25 @@
+import { useForm } from "react-hook-form"
+
 const EmailForm = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  function onSubmit() {
+    console.log('haha')
+  }
   return (
     <div className="emailForm">
-
+      <form
+        action="emailForm__form"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label htmlFor="firstName">
+          First name
+          <input type="sumbit" value={'haha'} name="firstName" />
+        </label>
+        <label htmlFor="lastName" className='flex flex-col'>
+          First name
+          <input type="sumbit" value={'haha'} name="lastName" />
+        </label>
+      </form>
     </div>
   )
 }
