ef3444d23a6aa304aeb0b23e2870bedf2d7b65e8
Yury Borets <udborets@gmail.com>
2023-04-05 17:14:21 +0300

    add contact us page form inputs prop

--- src/components/react/ContactUsForm/ContactUsForm.tsx
+++ src/components/react/ContactUsForm/ContactUsForm.tsx
@@ -1,9 +1,12 @@
 import { useForm } from "react-hook-form"
-import { useState } from 'react';
 
 import ContactUsFormInput from "@/components/react/ContactUsFormInput/ContactUsFormInput";
 
-const ContactUsForm = () => {
+export interface ContactUsFormProps {
+  inputsProps: { text: string, labelFor: string }[];
+}
+
+const ContactUsForm = ({ inputsProps }: ContactUsFormProps) => {
   const { register,
     formState: { errors },
     handleSubmit,
@@ -22,24 +25,9 @@ const ContactUsForm = () => {
         className="contactUsForm__form p-2 w-full h-full flex flex-col gap-2"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <ContactUsFormInput
-          labelFor="firstName"
-          register={register}
-          error={errors}
-          text={'First name'}
-        />
-        <ContactUsFormInput
-          labelFor="secondName"
-          register={register}
-          error={errors}
-          text={'Second name'}
-        />
-        <ContactUsFormInput
-          labelFor="email"
-          register={register}
-          error={errors}
-          text={'Email'}
-        />
+        {inputsProps.map((inputProps) => (
+          <ContactUsFormInput {...inputProps} error={errors} register={register} key={inputProps.labelFor} />
+        ))}
         <textarea
           className={'outline outline-1 focus:outline-2 focus:outline-black min-h-[100px] max-h-[200px] w-full'}
           {...register('messageText', {
