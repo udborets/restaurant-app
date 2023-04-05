71770fb81dd59e4680612a8cf3c6daaab3a1f1d8
Yury Borets <udborets@gmail.com>
2023-04-05 08:41:44 +0300

    fix input styles

--- src/components/react/ContactUsFormInput/ContactUsFormInput.tsx
+++ src/components/react/ContactUsFormInput/ContactUsFormInput.tsx
@@ -9,16 +9,25 @@ interface ContactUsFormInputProps {
 
 const ContactUsFormInput = ({ labelFor, register, error, text }: ContactUsFormInputProps) => {
   return (
-    <label htmlFor={labelFor} className='flex flex-col'>
-      <span>{text}</span>
+    <label htmlFor={labelFor} className='flex flex-col w-[200px] h-[90px]'>
+      <span
+        className="w-full text-left h-1/3"
+      >
+        {text}
+      </span>
       <input
         {...register(labelFor, {
           required: "This field can't be empty"
         })}
         name={labelFor}
+        className={`outline outline-1 focus:outline-2 focus:outline-black focus:shadow-sm w-full h-1/3 py-1 px-2`}
       />
       {error[labelFor]?.message
-        ? <div>{error[labelFor]?.message?.toString()}</div>
+        ? <div
+          className='h-1/3 text-red-500 w-full grid content-center'
+        >
+          {error[labelFor]?.message?.toString()}
+        </div>
         : ""}
     </label>
   )
