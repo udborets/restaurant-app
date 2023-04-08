4ba87616380179a29f9c02c2eb0b8fea8c403cd0
Yury Borets <udborets@gmail.com>
2023-04-08 17:33:26 +0300

    fix styles, add required text

--- src/components/react/ContactUsForm/ContactUsForm.tsx
+++ src/components/react/ContactUsForm/ContactUsForm.tsx
@@ -19,7 +19,7 @@ const ContactUsForm = ({ inputsProps }: ContactUsFormProps) => {
     reset();
   }
   return (
-    <div className="contactUsForm w-full sm:w-[500px] bg-black py-8 px-6 rounded-[15px]">
+    <div className="contactUsForm max-w-[600px] w-full bg-black py-8 px-6 rounded-[15px] relative">
       <form
         action="submit"
         className="contactUsForm__form p-2 w-full h-full flex flex-col gap-2"
@@ -30,10 +30,15 @@ const ContactUsForm = ({ inputsProps }: ContactUsFormProps) => {
         ))}
         <button
           type="submit"
-          className="contactUsForm__sendButton text-white px-4 py-3 bg-black outline-white outline outline-2 rounded-[8px] hover:bg-gray-900 transition-all duration-[.2s] ease-out"
+          className={`contactUsForm__sendButton text-white px-4 py-3 
+          bg-gray-800 outline-white outline-2 focus:outline active:outline-none outline-offset-2
+          rounded-[8px] hover:bg-gray-900 transition-colors duration-[.2s] ease-out text-[1.2rem] font-bold my-[1rem]`}
         >
-          Send message!
+          Send message
         </button>
+        <span className={`contactUsForm__requireHelpText absolute right-[20px] bottom-[20px] text-white`}>
+          <span className={`contactUsForm__ text-red-600`}>*</span> - required fields
+        </span>
       </form>
     </div>
   )
