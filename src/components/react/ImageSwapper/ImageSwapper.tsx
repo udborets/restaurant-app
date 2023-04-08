90a9609f47bd5cdfb3b7ebcb265a4672de5179c6
Yury Borets <udborets@gmail.com>
2023-04-08 17:38:11 +0300

    fix mapping indexes usage

--- src/components/react/ImageSwapper/ImageSwapper.tsx
+++ src/components/react/ImageSwapper/ImageSwapper.tsx
@@ -15,29 +15,29 @@ const ImageSwapper = ({ picturesNames }: ImageSwapperProps) => {
   }, 5000)
   return (
     <div className={`imageSwapper overflow-hidden h-[500px] lg:h-[600px] relative grid content-center bg-black`}>
-      {picturesNames.map((src, i) => (
+      {picturesNames.map((src, index) => (
         <img
           src={src}
           alt="pic"
           loading="lazy"
-          key={i}
-          className={`imageSwapper__img z-[0] object-cover w-full h-full absolute top-0 right-0 ${currentImgIndex === i ? 'opacity-100' : 'opacity-0'} duration-[1.3s] transition-all `}
+          key={Math.random()}
+          className={`imageSwapper__img z-[0] object-cover w-full h-full absolute top-0 right-0 ${currentImgIndex === index ? 'opacity-100' : 'opacity-0'} duration-[1.3s] transition-all `}
         />
       ))}
       <div
         className="imageSwapper__imageSelector outline-white outline-2 outline-double md:outline-none flex flex-col w-fit gap-4 absolute self-center 
         right-[10px] bg-opacity-100 md:bg-opacity-0 bg-black p-2 rounded-[10px]">
-        {picturesNames.map((_, i) => (
+        {picturesNames.map((_, index) => (
           <div
-            key={i}
+            key={Math.random()}
             onClick={() => {
               clearTimeout(imageSwapTimeout);
-              setCurrentImgIndex(i);
+              setCurrentImgIndex(index);
             }}
             className="imageSwapper__imageSelect p-1 z-[10] border-white border-[1px] rounded-[50%] shadow-white hover:bg-white transition-all duration-500"
           >
             <div
-              className={`${currentImgIndex === i ? "bg-white" : ""} w-2 h-2 rounded-[50%] transition-all duration-500`}
+              className={`${currentImgIndex === index ? "bg-white" : ""} w-2 h-2 rounded-[50%] transition-all duration-500`}
             />
           </div>
         ))}
