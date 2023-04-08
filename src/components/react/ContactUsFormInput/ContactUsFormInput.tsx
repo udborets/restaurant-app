d62cc5baf1a89cdbd1a720589a336dbec80e4f18
Yury Borets <udborets@gmail.com>
2023-04-08 16:41:23 +0300

    fix styles

--- src/components/react/ContactUsFormInput/ContactUsFormInput.tsx
+++ src/components/react/ContactUsFormInput/ContactUsFormInput.tsx
@@ -1,4 +1,5 @@
 import type { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import styles from "./ContactUsFormInput.module.scss";
 
 export interface ContactUsFormInputProps {
   labelFor: string;
@@ -10,9 +11,9 @@ export interface ContactUsFormInputProps {
 
 const ContactUsFormInput = ({ labelFor, register, error, text, isRequiredText }: ContactUsFormInputProps) => {
   return (
-    <label htmlFor={labelFor} className='flex flex-col w-full h-[90px]'>
+    <label htmlFor={labelFor} className='contactUsFromInput flex flex-col w-full h-[90px]'>
       <span
-        className="w-full text-left h-1/3 text-white"
+        className={`contactUsFormInput__fieldName relative text-xl ${isRequiredText ? styles.required : ""} w-full text-left h-1/3 text-white`}
       >
         {text}
       </span>
@@ -21,11 +22,12 @@ const ContactUsFormInput = ({ labelFor, register, error, text, isRequiredText }:
           required: isRequiredText ?? false
         })}
         name={labelFor}
-        className={`focus:outline rounded-[8px] focus:outline-2 text-black bg-slate-200 focus:bg-white outline-white outline-offset-2 w-full h-1/3 py-1 px-2`}
+        className={`contactUsFormInput__input focus:outline rounded-[8px] focus:outline-2 
+        bg-slate-200 focus:bg-white outline-white outline-offset-2 w-full h-1/3 py-1 px-2`}
       />
       {error[labelFor]?.message
         ? <div
-          className='h-1/3 text-red-500 w-full grid content-center'
+          className='contactUsFormInput__errorMessage h-1/3 text-red-500 w-full grid content-center'
         >
           {error[labelFor]?.message?.toString()}
         </div>
